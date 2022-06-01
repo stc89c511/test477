7337c82f55f0258261b897c54e1edd94b8b94ade
zhangsan <271193851@qq.com>
2022-06-01 16:47:32 +0800

    test477初始版本

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!F:\Users\Admin\PycharmProjects\djangoProject\test477\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
